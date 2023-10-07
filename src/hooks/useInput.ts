5b3ced30fffa031ffd528589d213067a66250321
devseop <cocc263@gmail.com>
2023-10-07 18:26:45 +0900

    :art: FEAT: 전화번호 입력시 자동 하이픈 삽입 로직 추가

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -6,10 +6,22 @@ export const useInput = <T extends Record<string, string>>(initailValue: T) => {
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+    let processedValue = value;
+
+    if (name === "phone") {
+      if (value.length === 10) {
+        processedValue = value.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+      }
+      if (value.length === 13) {
+        processedValue = value
+          .replace(/-/g, "")
+          .replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3");
+      }
+    }
 
     setValues((prevValues) => ({
       ...prevValues,
-      [name]: value,
+      [name]: processedValue,
     }));
   };
 
