5ac85627b1793f918860b61c688d6010a247e743
devseop <cocc263@gmail.com>
2023-10-07 17:51:37 +0900

    :art: FEAT: 라우터 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../pages/App";
 import { ProductDetail } from "../components/product/ProductDetail";
 import { SignIn } from "../components/user/SignIn";
+import { UserInfoForm } from "../components/buy/UserInfoForm";
 
 export const Router = () => {
   return (
@@ -9,7 +10,8 @@ export const Router = () => {
       <Routes>
         <Route path="/products" element={<App />} />
         <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/buy/checkInfo" element={<UserInfoForm />} />
       </Routes>
     </BrowserRouter>
   );
